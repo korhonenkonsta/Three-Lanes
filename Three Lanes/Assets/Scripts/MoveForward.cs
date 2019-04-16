a7874550bf894a110de348c77250b46b96230329
korhonenkonsta <korhonenkk@gmail.com>
2019-04-17 00:28:45 +0300

    unity 2019, spawning

---
+++ Three Lanes/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float factor;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + transform.forward * Time.deltaTime * factor);
+    }
+}
---
+++ Three Lanes/Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefabToSpawn;
+    public bool doSpawn = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Example());
+    }
+
+    IEnumerator Example()
+    {
+        while (doSpawn)
+        {
+            Spawn(prefabToSpawn);
+            yield return new WaitForSeconds(5);
+        }
+    }
+
+    public void Spawn(GameObject prefab)
+    {
+        Instantiate(prefab, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
