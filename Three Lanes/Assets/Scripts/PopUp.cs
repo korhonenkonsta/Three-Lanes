a5bfe670aef09ffea1209b9200038a9f1c4035ea
korhonenkonsta <korhonenkk@gmail.com>
2019-08-15 22:18:35 +0300

    resource popup

---
+++ Three Lanes/Assets/Scripts/PopUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUp : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + Vector3.up, moveSpeed * Time.deltaTime);
+    }
+}
--- Three Lanes/Assets/Scripts/ResourceGenerator.cs
+++ Three Lanes/Assets/Scripts/ResourceGenerator.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ResourceGenerator : MonoBehaviour
 {
     public float resourceInterval = 5;
+    public int resourceAmount = 1;
     public bool doGenerate = true;
     public Player owner;
     public Building b;
+
+    public GameObject resourcePopupPrefab;
+    public float height = 1f;
 
     void Start()
     {
@@ -21,12 +27,20 @@ public class ResourceGenerator : MonoBehaviour
         StartCoroutine(ContinuousGenerate());
     }
 
+    public void CreatePopup()
+    {
+        GameObject popup = Instantiate(resourcePopupPrefab, transform.position + new Vector3(0, height, 0), Quaternion.identity);
+        popup.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "+" + resourceAmount.ToString();
+        Destroy(popup, 3f);
+    }
+
     IEnumerator ContinuousGenerate()
     {
         while (doGenerate)
         {
             yield return new WaitForSeconds(resourceInterval);
-            owner.roundExtraResources++;
+            owner.roundExtraResources += resourceAmount;
+            CreatePopup();
         }
     }
 
